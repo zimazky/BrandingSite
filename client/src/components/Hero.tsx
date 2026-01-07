f778a605961dd0c45d41e83482f0c61a6f1a6ffe
andrew zimatskiy <zimazky@gmail.com>
2026-01-08 00:34:51 +0300

    Компетенции

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -8,26 +8,41 @@ const Hero = () => {
           <h1 className="font-pt-sans-narrow text-[6vw] md:text-[5vw] lg:text-[5vw] font-bold leading-none uppercase text-black [word-spacing:-2vw]">
             Разработчик /<br/>Специалист по автоматизации
           </h1>
-
         </div>
-        <div className="flex justify-center mt-16">
-            <div className="w-128 h-160 relative overflow-hidden">
-                <Image
-                src="/img/me.jpg"
-                alt="Зимацкий Андрей"
-                layout="fill"
-                objectFit="cover"
-                />
+        <div className="mt-16 flex justify-center">
+            <div className="flex items-end gap-x-8">
+                <div className="font-mono text-sm uppercase">
+                    <p>/ Automation Specialist</p>
+                    <p>/ Software Development</p>
+                    <p>/ C#/.NET</p>
+                    <p>/ ASP.NET Core</p>
+                    <p>/ WPF, Avalonia UI</p>
+                    <p>/ Entity Framework Core</p>
+                    <p>/ SQL MSSQL PostgreSQL</p>
+                    <p>/ SignalR</p>
+                    <p>/ Camunda BPM</p>
+                    <p>/ Docker</p>
+                    <p>/ Web Development</p>
+                    <p>/ TypeScript/JavaScript HTML CSS</p>
+                    <p>/ React Next.js</p>
+                    <p>/ Python</p>
+                    <p>/ Development with AI Tools</p>
+                    <p>/ Gemini CLI</p>
+                    <p>/ Claude Code</p>
+                    <p>/ Github Copilot</p>
+                    <p>/ Cursor</p>
+                    <p>/ Qoder</p>
+                </div>
+  
+                <div className="relative h-160 w-128">
+                    <Image
+                    src="/img/me.jpg"
+                    alt="Зимацкий Андрей"
+                    layout="fill"
+                    objectFit="cover"
+                    />
+                </div>
             </div>
-        </div>
-        <div className="flex justify-between items-end mt-16">
-          <div className="font-mono text-sm uppercase">
-            <p>/ C#/.NET</p>
-            <p>/ Python</p>
-          </div>
-          <div className="font-mono text-sm uppercase text-right">
-            <p>Based in Saint-Petersburg</p>
-          </div>
         </div>
       </div>
     </section>
